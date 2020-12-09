5e398f689cdee27cf2d914579baa20d9e7fd3fd8
mihaiconstantin <mihai.a.constantin@gmail.com>
2020-12-09 17:06:47 +0100

    Added helper file

---
+++ scripts/helpers.R
@@ -0,0 +1,9 @@
+# # #
+# The role of this file is to load all dependencies needed to run the analysis.
+# It can be sourced wherever needed.
+# # #
+
+# # # Load the libraries
+library(ggplot2)
+library(gridExtra)
+
